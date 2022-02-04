8fb452218abb9f41c9e4e92c571acacd124ead91
Debanjan Sarkar <debanjan02sarkar@outlook.com>
2022-02-05 01:09:41 +0530

    Update

--- CamelTest/src/main/java/me/tests/camel/config/AppConfig.java
+++ CamelTest/src/main/java/me/tests/camel/config/AppConfig.java
@@ -1,6 +1,8 @@
 package me.tests.camel.config;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spi.ThreadPoolProfile;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 
 @Configuration
 public class AppConfig {
@@ -23,6 +26,31 @@ public class AppConfig {
             public void configure() throws Exception {
                 from("file://C://GIT//input1").to("file://C://GIT//output1");
             }
+        };
+    }
+
+    @Bean
+    RoutesBuilder produceToSedaRoute1() {
+        return new RouteBuilder() {
+          @Override
+          public void configure() throws Exception {
+              from("timer://simpleTimer?period=1000").process(new Processor() {
+                          @Override
+                          public void process(Exchange exchange) throws Exception {
+                            String body = "Hello from timer at " + new Date();
+                            exchange.getIn().setBody(body);
+                          }
+                      })
+                      //.setBody(simple("Hello from timer at " + new Date()))
+                      .to("seda:out1");
+
+              from("seda:out1").process(new Processor() {
+                  @Override
+                  public void process(Exchange exchange) throws Exception {
+                      System.out.println("Message read from seda: " + (exchange.getIn().getBody()).toString());
+                  }
+              });
+          }
         };
     }
 
