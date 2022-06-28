76c68c7e2b9a20b838486cf36c6fb09b97463aab
Debanjan Sarkar <debanjan02sarkar@outlook.com>
2022-06-28 21:11:50 +0530

    Fixed

--- CamelTest/src/main/java/me/tests/camel/component/PostSplitProcessor.java
+++ CamelTest/src/main/java/me/tests/camel/component/PostSplitProcessor.java
@@ -2,6 +2,7 @@ package me.tests.camel.component;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.support.DefaultExchange;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,7 @@ public class PostSplitProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        System.out.println("Internal Properties: " + ((DefaultExchange) exchange).getInternalProperties());
         System.out.println("Properties: " + exchange.getProperties());
         System.out.println("Headers: " + exchange.getIn().getHeaders());
         System.out.println("Body: " + exchange.getIn().getBody());
